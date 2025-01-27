a6d31b899168b095983738f6b2b150a6c6666e94
rabbai007 <amaluobinna@gmail.com>
2025-01-27 10:32:10 -0500

    plugins

--- scripts/setup-lab.sh
+++ scripts/setup-lab.sh
@@ -20,3 +20,21 @@ cd
 mkdir .ssh && chmod 700 .ssh
 touch .ssh/authorized_keys && chmod 600 .ssh/authorized_keys
 
+
+# Start the Docker containers
+cd ~
+mkdir jenkins_home
+mkdir jenkins_home/plugins
+cp -R lab/plugins/* jenkins_home/plugins/
+
+# Jenkins
+sudo docker run -p 8080:8080 -p 50000:50000 --restart=on-failure -v /home/pslearner/jenkins_home:/var/jenkins_home jenkins/jenkins:lts-jdk17
+
+# Prometheus
+sudo docker run -p 9090:9090 -v /home/pslearner/lab/configs/prometheus.yml:/etc/prometheus/prometheus.yml -v /home/pslearner/prometheus-data:/prometheus prom/prometheus
+    
+# Grafana
+sudo docker run -d --name grafana -p 3000:3000 grafana/grafana
+
+# Nagios
+sudo docker run --name nagios -p 0.0.0.0:8081:80 jasonrivers/nagios:latest	
