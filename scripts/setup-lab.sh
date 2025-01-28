bf2bf9db265c854180035b9ab09d1717b26d498d
rabbai007 <amaluobinna@gmail.com>
2025-01-28 10:56:07 -0500

    Setup-lab.sh file

--- scripts/setup-lab.sh
+++ scripts/setup-lab.sh
@@ -7,34 +7,19 @@ ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 
 # Git Server Setup
 echo -e "Setting up the local Git Server.....\n"
+
+# Creating the git user and all the necessary filesystem and permissions
 sudo useradd -m -p $(openssl passwd -5 'password1') git
-su git
-mkdir .ssh && chmod 700 .ssh
-sudo mkdir /usr/local/git
-sudo chown git:git /usr/local/git
-
-
-
-su git
+sudo su - git
 cd
 mkdir .ssh && chmod 700 .ssh
 touch .ssh/authorized_keys && chmod 600 .ssh/authorized_keys
+mkdir repositories
+mkdir repositories/cicd-jenkins.git
+chmod -R 777 repositories
 
+# Initializing the git repo
+cd repositories/cicd-jenkins.git
+git init --bare
 
-# Start the Docker containers
-cd ~
-mkdir jenkins_home
-mkdir jenkins_home/plugins
-cp -R lab/plugins/* jenkins_home/plugins/
-
-# Jenkins
-sudo docker run -p 8080:8080 -p 50000:50000 --restart=on-failure -v /home/pslearner/jenkins_home:/var/jenkins_home jenkins/jenkins:lts-jdk17
-
-# Prometheus
-sudo docker run -p 9090:9090 -v /home/pslearner/lab/configs/prometheus.yml:/etc/prometheus/prometheus.yml -v /home/pslearner/prometheus-data:/prometheus prom/prometheus
-    
-# Grafana
-sudo docker run -d --name grafana -p 3000:3000 grafana/grafana
-
-# Nagios
-sudo docker run --name nagios -p 0.0.0.0:8081:80 jasonrivers/nagios:latest	
+echo "Your git repo can be cloned using 'git clone git@localhost:/home/git/repositories/cicd-jenkins.git'"
