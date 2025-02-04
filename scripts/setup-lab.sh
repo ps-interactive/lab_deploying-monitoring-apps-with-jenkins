0af0baeac9e831a27e72117bde310ef8ee4d48c4
rabbai007 <amaluobinna@gmail.com>
2025-02-04 09:15:07 -0500

    setup scripts

--- scripts/setup-lab.sh
+++ scripts/setup-lab.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-## This script is used to setup the lab environment and get all the required credentials needed.
+# Variables
+JENKINS_ADMIN_PASSWD=$(cat /var/jenkins_home/secrets/initialAdminPassword)
 
-# Generate the ssh key for pslearner user
-# ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+## Setting up the lab and retrieving the credentials.
 
-# Git Server Setup
-# echo -e "Setting up the local Git Server.....\n"
-
-# Creating the git user and all the necessary filesystem and permissions
-# sudo useradd -m -p $(openssl passwd -5 'password1') git
-# sudo su - git
-# cd
-# mkdir .ssh && chmod 700 .ssh
-# touch .ssh/authorized_keys && chmod 600 .ssh/authorized_keys
-# mkdir repositories
-# mkdir repositories/cicd-jenkins.git
-# chmod -R 777 repositories
-
-# Initializing the git repo
-# cd repositories/cicd-jenkins.git
-# git init --bare
-
-# echo "Your git repo can be cloned using 'git clone git@localhost:/home/git/repositories/cicd-jenkins.git'"
+# Clone your got repo
+echo "Your git repo has be cloned using 'git clone git@localhost:/home/git/repositories/cicd-jenkins.git' to /home/pslearner/cicd-jenkins"
 git clone git@localhost:/home/git/repositories/cicd-jenkins.git
+
+# Set the global variables for git
+git config --global user.email "pslearner@example.com"
+git config --global user.name "PSLearner"
+
+echo "Your Git configs for user email and name have been set to email - pslearner@example.com and name - PSLearner"
+
+# Ensure you copy your SSH key to the git user to allow passwordless git actions (like push, pull etc)
+echo "Use 'ssh-copy-id git@localhost' to copy your ssh key to the git user"
+
+# Credentials listing
+# Git
+echo "Git user password is 'git'"
+echo "Jenkins URL is 'http://localhost:3000'"
+echo "Prometheus URL is 'http://localhost:9090'"
+echo "Grafana URL is 'http://localhost:3000'"
+echo "Nagios URL is 'http://localhost:8081'"
+echo "Jenkins admin password is $JENKINS_ADMIN_PASSWD"
