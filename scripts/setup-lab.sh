cda8a74e334b6acf7e770dc79aa11f72bc7c6b5e
rabbai007 <amaluobinna@gmail.com>
2025-02-05 22:23:31 -0500

    setup scripts

--- scripts/setup-lab.sh
+++ scripts/setup-lab.sh
@@ -2,6 +2,10 @@
 
 # Variables
 JENKINS_ADMIN_PASSWD=$(sudo cat /home/pslearner/jenkins_home/secrets/initialAdminPassword)
+JENKINS_IP=`sudo docker inspect $(sudo docker ps -a | grep jenkins | awk '{print $1}') | grep IPAddress | cut -d":" -f 2 | grep -v null | uniq | cut -d'"' -f 2`
+PROMETHEUS_IP=`sudo docker inspect $(sudo docker ps -a | grep prometheus | awk '{print $1}') | grep IPAddress | cut -d":" -f 2 | grep -v null | uniq | cut -d'"' -f 2`
+GRAFANA_IP=`sudo docker inspect $(sudo docker ps -a | grep grafana | awk '{print $1}') | grep IPAddress | cut -d":" -f 2 | grep -v null | uniq | cut -d'"' -f 2`
+NAGIOS_IP=`sudo docker inspect $(sudo docker ps -a | grep nagios | awk '{print $1}') | grep IPAddress | cut -d":" -f 2 | grep -v null | uniq | cut -d'"' -f 2`
 
 ## Setting up the lab and retrieving the credentials.
 
@@ -22,8 +26,13 @@ echo "++         LAB DETAILS/CREDENTIALS            ++"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++"   
 echo ""
 echo "Git user password is 'git'"
-echo "Jenkins URL is 'http://localhost:3000'"
-echo "Prometheus URL is 'http://localhost:9090'"
-echo "Grafana URL is 'http://localhost:3000'"
-echo "Nagios URL is 'http://localhost:8081'"
+echo "Jenkins URL is 'http://$JENKINS_IP:3000'"
+echo "Prometheus URL is 'http://$PROMETHEUS_IP:9090'"
+echo "Grafana URL is 'http://$GRAFANA_IP:3000'"
+echo "Nagios URL is 'http://$NAGIOS_IP:80'"
+echo ""
+echo "LAB LOGIN DETAILS"
+echo "+++++++++++++++++"
 echo "Jenkins admin password is $JENKINS_ADMIN_PASSWD"
+echo "Grafana username/password is admin/admin. You can skip the password change"
+echo "Nagios username/password is nagiosadmin/nagios."
